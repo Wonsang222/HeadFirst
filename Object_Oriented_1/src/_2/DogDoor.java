d71294f33ba0e9d2b3d117c9d057a36a8936a99e
Benny <wonsang222@naver.com>
2023-12-10 21:35:22 +0900

    day six, seven done

--- Object_Oriented_1/src/_2/DogDoor.java
+++ Object_Oriented_1/src/_2/DogDoor.java
@@ -1,7 +1,33 @@
 package _2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoor {
     private boolean open;
+
+    private Bark allowedBark;
+
+    private ArrayList<Bark> allowedBarks = new ArrayList<Bark>();
+
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public Bark getAllowedBark() {
+        return allowedBark;
+    }
+
+    public void addAllowedBark(Bark allowedBark) {
+        allowedBarks.add(allowedBark);
+    }
+
+    public ArrayList<Bark> getAllowedBarks() {
+        return allowedBarks;
+    }
 
     public DogDoor() {
         this.open = false;
@@ -10,6 +36,15 @@ public class DogDoor {
     public void open() {
         System.out.println("The dog door open");
         open = true;
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                close();
+                timer.cancel();
+            }
+        }, 5000);
     }
 
     public void close() {
@@ -20,7 +55,4 @@ public class DogDoor {
     public boolean isOpen() {
         return open;
     }
-
-
-
 }
--- Object_Oriented_1/src/_2/DogDoorSimulator.java
+++ Object_Oriented_1/src/_2/DogDoorSimulator.java
@@ -3,14 +3,19 @@ package _2;
 public class DogDoorSimulator {
     public static void main(String[] args) {
         DogDoor door = new DogDoor();
+        BarkRecognizer recognizer = new BarkRecognizer(door);
         Remote remote = new Remote(door);
-        System.out.println("Fido barks to go outside");
+
+
+
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {
+
+        }
+
         remote.pressButton();
-        System.out.println("\nFido has gone outside.." );
-
-        System.out.println("\nFido all done" );
-
-        System.out.println("\nFido barks inside" );
+        System.out.println("\nFido is back inside" );
 
 
     }
